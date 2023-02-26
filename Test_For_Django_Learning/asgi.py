77fcd6682737e61f4235a34734da39511d2f6987
wxbj <weizhen20010616@163.com>
2023-02-26 11:45:53 +0800

    daily

---
+++ Test_For_Django_Learning/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Test_For_Django_Learning project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test_For_Django_Learning.settings')
+
+application = get_asgi_application()
